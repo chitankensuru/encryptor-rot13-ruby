0977a541437d6757713c5b65413b609454e204f6
Ritchy <chitankensuru@gmail.com>
2015-05-05 20:13:30 +0200

    Add file encryption and decryption with some test files

--- encryptor.rb
+++ encryptor.rb
@@ -1,30 +1,52 @@
 class Encryptor
+
+  def supported_characters
+    (' '..'z').to_a
+  end
+
   def cipher(rotation)
-    characters = (' '..'z').to_a
+    characters = supported_characters
     rotated_characters = characters.rotate(rotation)
     Hash[characters.zip(rotated_characters)]
   end
 
-  def rotate_cipher(rotation)
-    cipher(rotation)
-  end
-
   def encrypt_letter(letter, rotation)
-    rotate_cipher(rotation)[letter]
+    cipher(rotation)[letter]
   end
 
   def decrypt_letter(letter, rotation)
-    rotate_cipher(rotation).key(letter)
+    cipher(rotation).key(letter)
   end
 
-
   def encrypt(string, rotation)
-    string.split("").each_with_object("") { |letter, str| str <<
-        encrypt_letter(letter, rotation) }
+    string.split("").collect { |letter|
+      decrypt_letter(letter, rotation)
+    }.join
   end
 
   def decrypt(string, rotation)
-    string.split("").each_with_object("") { |letter, str| str <<
-        decrypt_letter(letter, rotation) }
+    string.split("").collect { |letter|
+      decrypt_letter(letter, rotation)
+    }.join
+  end
+
+  def encrypt_file(filename, rotation)
+    input = File.open(filename, "r")
+    encrypted_input = encrypt(input.read, rotation)
+    input.close
+    output = File.open(filename + ".secret", "w")
+    output.write(encrypted_input)
+    output.close
+  end
+
+  def decrypt_file(filename)
+    input = File.open(filename, "r")
+    encrypted_input = input.read
+    output = File.open(filename + ".decrypt", "w")
+    supported_characters.count.times.collect do |attempt|
+      output.write(decrypt(encrypted_input, attempt) + "\n")
+    end
+    input.close
+    output.close
   end
 end
